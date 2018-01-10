e7845de829661fa237708071e9ddbf2363f0cf71
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-09 23:05:51 -0800

    Initial Commit

---
+++ Ford Vehicle Collection/Model/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  UI Fun
+//
+//  Created by user on 1/9/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Vehicle {
+    let pic: UIImage
+    let year: Int
+    let name: String
+    let price: String
+}
---
+++ Ford Vehicle Collection/View/Cars/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UI Fun
+//
+//  Created by user on 1/6/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+   
+    
+}
+
