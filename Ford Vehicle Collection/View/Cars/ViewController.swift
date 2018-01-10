3820cd5231276368960fab4b6716b39b5d415eea
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-10 01:00:50 -0800

    Code&UI: Begin UI setup of CollectionView and CollectionViewCell in CarsView. Will use as template for other viewControllers

--- Ford Vehicle Collection/View/Cars/ViewController.swift
+++ Ford Vehicle Collection/View/Cars/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var CarCollection = [Vehicle]()
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    let Fiesta = Vehicle.init(pic:#imageLiteral(resourceName: "Fiesta") , year: 2018, name: "Fiesta", price: "From $14,115 ")
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return CarCollection.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
 
   
     override func viewDidLoad() {
