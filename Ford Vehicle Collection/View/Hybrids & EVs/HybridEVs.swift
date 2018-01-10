d3953e11acc36b5227f9a2e02964c824f3a12f90
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-09 23:12:20 -0800

    Setup: project organization, setup folders, create Model

---
+++ Ford Vehicle Collection/View/Hybrids & EVs/HybridEVs.swift
@@ -0,0 +1,15 @@
+//
+//  HybridEVs.swift
+//  Ford Vehicle Collection
+//
+//  Created by user on 1/9/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HybridEVs: UIViewController {
+    
+    
+}
---
+++ Ford Vehicle Collection/View/Performance Vehicles/Performance Vehicles.swift
@@ -0,0 +1,15 @@
+//
+//  Performance Vehicles.swift
+//  Ford Vehicle Collection
+//
+//  Created by user on 1/9/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PerformanceVehicles: UIViewController {
+    
+    
+}
