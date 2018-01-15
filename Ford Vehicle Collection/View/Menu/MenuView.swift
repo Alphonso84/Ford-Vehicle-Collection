f0a57c3d48e2a1fd700b5867d941edddfa377876
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-15 05:08:10 -0800

    Add: Added menu and buttons

---
+++ Ford Vehicle Collection/View/Menu/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  Ford Vehicle Collection
+//
+//  Created by user on 1/15/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Menu: UIViewController {
+    @IBAction func userDidTap(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
